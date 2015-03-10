19f128b334ed2a45437ae6f9a28e57524e3d3697
jrezuke <j.rezuke@verizon.net>
2015-03-10 10:28:17 -0400

    added angular bootstrap

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var app = angular
-        .module('app', ['ui.router']);
+        .module('app', ['ui.router', 'ui.bootstrap']);
 
     app.config(function ($stateProvider, $urlRouterProvider) {
 
