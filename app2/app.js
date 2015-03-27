940cca12f0c725b0a54c7c5b0fb3830aa7332cf0
j.rezuke <j.rezuke@verizon.net>
2015-03-27 08:11:34 -0400

    working on popover

--- app2/app.js
+++ app2/app.js
@@ -6,9 +6,9 @@
 
     app.controller('TestCtrl', function(){
         var vm = this;
-        vm.items2 = [{name: 'one', age: 10, othersId:3 },
-            { name: 'two', age: 20, othersId:2 },
-            { name: 'three', age: 30, othersId:1 }];
+        vm.items2 = [{id:1, name: 'one', age: 10, othersId:3 },
+            {id:2, name: 'two', age: 20, othersId:2 },
+            {id:3, name: 'three', age: 30, othersId:1 }];
         vm.items = ['one','two','three','four']
     })
 
